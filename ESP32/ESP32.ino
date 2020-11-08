808bbe9eaf7a59477497b2069bbeb953aa69e468
haavardok <havris97@gmail.com>
2020-11-08 10:52:57 +0100

    Minor cleanup in comments.

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,23 +1,21 @@
 #include <analogWrite.h> //Import the analogWrite library for ESP32 so that analogWrite works properly
-
-#include <WiFi.h>//Imports the needed WiFi libraries
-#include <WiFiMulti.h> //We need a second one for the ESP32 (these are included when you have the ESP32 libraries)
-
+#include <WiFi.h>        //Imports the needed WiFi libraries
+#include <WiFiMulti.h>   //We need a second one for the ESP32 (these are included when you have the ESP32 libraries)
 #include <SocketIoClient.h> //Import the Socket.io library, this also imports all the websockets
 
-WiFiMulti WiFiMulti; //Declare an instane of the WiFiMulti library
+WiFiMulti WiFiMulti; //Declare an instance of the WiFiMulti library
 SocketIoClient webSocket; //Decalre an instance of the Socket.io library
 
 void event(const char * payload, size_t length) { //Default event, what happens when you connect
   Serial.printf("got message: %s\n", payload);
 }
 
-void changeLEDState(const char * LEDStateData, size_t length) { //What happens when the ESP32 receives a instruction from the server (with variable) to change the LED
+void changeLEDState(const char * LEDStateData, size_t length) { //What happens when the ESP32 receives an instruction from the server (with variable) to change the LED
   Serial.printf("LED State: %s\n", LEDStateData); //First we print the data formated with the "printf" command
-  Serial.println(LEDStateData); //Then we just print the LEDStateData which will be a int (0 og 1 so in reeality bool) that tells us what to do with the LED
+  Serial.println(LEDStateData); //Then we just print the LEDStateData which will be a int (0 og 1 so in reality bool) that tells us what to do with the LED
 
   //Data conversion //We need som data processing to make this work
-  String dataString(LEDStateData); //First we convert the const char array(*) to a string in Arduino (this makes thing easier)
+  String dataString(LEDStateData); //First we convert the const char array(*) to a string in Arduino (this makes things easier)
   int LEDState = dataString.toInt(); //When we have a string we can use the built in Arduino function to convert to an integer
 
   Serial.print("This is the LED state in INT: "); //We print the final state
@@ -96,13 +94,13 @@ void setup() {
     Serial.println();
     Serial.println();
 
-      for(uint8_t t = 4; t > 0; t--) { //More debugging
+      for(uint8_t t = 4; t > 0; t--) { //Wait four seconds to boot the ESP32
           Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
           Serial.flush();
           delay(1000);
       }
 
-    WiFiMulti.addAP("SSID", "passwd"); //Add a WiFi hotspot (addAP = add AccessPoint) (put your own network name and password in here)
+    WiFiMulti.addAP("SSID", "passwd"); //Add a WiFi hotspot (addAP = add AccessPoint)
 
     while(WiFiMulti.run() != WL_CONNECTED) { //Here we wait for a successfull WiFi connection untill we do anything else
       Serial.println("Not connected to wifi...");
@@ -112,7 +110,7 @@ void setup() {
     Serial.println("Connected to WiFi successfully!"); //When we have connected to a WiFi hotspot
 
     //Here we declare all the different events the ESP32 should react to if the server tells it to.
-    //a socket.emit("identifier", data) with any of the identifieres as defined below will make the socket call the functions in the arguments below
+    //A socket.emit("identifier", data) on the server, with any of the identifieres as defined below will make the socket call the functions in the arguments below
     webSocket.on("clientConnected", event); //For example, the socket.io server on node.js calls client.emit("clientConnected", ID, IP) Then this ESP32 will react with calling the event function
     webSocket.on("LEDStateChange", changeLEDState);
     webSocket.on("DriveStateChange", changeDriveState);
@@ -122,7 +120,7 @@ void setup() {
     //Send data to server/webpage
     webSocket.on("dataRequest", dataRequest); //Listens for the command to send data
 
-    webSocket.begin("91.192.221.40", 2520); //This starts the connection to the server with the ip-address/domainname and a port (unencrypted)
+    webSocket.begin("192.168.1.158", 2520); //This starts the connection to the server with the ip-address/domainname and a port (unencrypted)
 }
 
 //Drive the car forwards or backwards (THIS IS JUST AN EXAMPLE AND NOT WHAT YOU HAVE TO USE IT FOR)
